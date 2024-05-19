5caa30301da0945cdfb3f1528e23acb7fdf60136
yoesak <yusak.setiawan@tigabelas.com>
2024-05-19 20:13:41 +0700

    add file for training common voice id

--- test_finetune.py
+++ test_finetune.py
@@ -7,19 +7,21 @@ import numpy as np
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 repo_id = "parler-tts/parler_tts_mini_v0.1"
+feature_extractor_repo_id = "ylacombe/dac_44khZ_8kbps"
+tokenizer_repo_id = "google/flan-t5-base"
 
-model = ParlerTTSForConditionalGeneration.from_pretrained("/home/ys/project/parler-tts/output_dir_training_indo", torch_dtype=torch.float16).to(device)
-tokenizer = AutoTokenizer.from_pretrained(repo_id)
-feature_extractor = AutoFeatureExtractor.from_pretrained(repo_id)
+model = ParlerTTSForConditionalGeneration.from_pretrained("/home/ys/project/parler-tts/output_dir_training_id_male", torch_dtype=torch.float32).to(device)
+tokenizer = AutoTokenizer.from_pretrained("/home/ys/project/parler-tts/output_dir_training_id_male")
+feature_extractor = AutoFeatureExtractor.from_pretrained("/home/ys/project/parler-tts/output_dir_training_id_male")
 
 
 prompt = "Sejak itu mereka hidup damai di Bulan."
-description = "an indonesian male with a loud voice speaks in a quiet room with moderate ambient sound, with some expressive delivery."
+description = "an indonesian male"
 
 input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
 prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
 
-generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
+generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids, do_sample=True, temperature=1.0)
 audio_arr = generation.cpu().numpy().squeeze()
 
 audio_array_int16 = (audio_arr * np.iinfo(np.int16).max).astype(np.int16)
